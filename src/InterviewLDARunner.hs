55e9cf610b76da083481bc1a14074768bc24a07a
Balazs <ga87ken>
2022-02-23 10:06:41 +0100

    add functions

--- src/InterviewLDARunner.hs
+++ src/InterviewLDARunner.hs
@@ -4,6 +4,7 @@ import Configuration
 import Control.Concurrent.ParallelIO.Global (parallel_)
 import Data.Functor ((<&>))
 import Data.List (intercalate, nub)
+import qualified Data.Map as Map
 import Debug
 import Document
 import GHC.IO.Encoding (setLocaleEncoding)
@@ -11,6 +12,24 @@ import InterviewReader
 import LDARunner hiding (documents, numberOfTopics, saveInterval, saveIterations, seed)
 import System.Directory.Internal.Prelude (catMaybes)
 import System.IO
+
+runLDAOnCombinedFullDataSet :: IO ()
+runLDAOnCombinedFullDataSet = readInterviews interviewDirectory >>= processInterviews
+  where
+    processInterviews interviews = resultDirectory fullDataSet >>= flip processInterviewsToFiles interviews
+
+    processInterviewsToFiles fileName = sequence_ . writeToFile fileName . runLDAOnInterviews
+
+    writeToFile fileName models =
+      setLocaleEncoding utf8 :
+      writeFile fileName fullDataSet :
+      fmap (appendFile fileName . showResult) models
+
+    runLDAOnInterviews = runLDA . singletonList . combineDocuments "Full-Data-Set" . createDocuments 
+    
+    singletonList x = [x]
+
+    runLDA documents = run $ Input documents saveIterations saveInterval numberOfTopics seed
 
 runLDAOnFullDataSet :: IO ()
 runLDAOnFullDataSet = readInterviews interviewDirectory >>= processInterviews
@@ -25,6 +44,28 @@ runLDAOnFullDataSet = readInterviews interviewDirectory >>= processInterviews
       fmap (appendFile fileName . showResult) models
 
     runLDAOnInterviews = runLDA . createDocuments
+
+    runLDA documents = run $ Input documents saveIterations saveInterval numberOfTopics seed
+
+runLDAPerCombinedSubfolder :: IO ()
+runLDAPerCombinedSubfolder = readInterviewsPerSubfolder interviewDirectory >>= runParallel
+  where
+    runParallel interviews = parallel_ $ fmap processInterviews interviews
+
+    processInterviews interviews = resultDirectory groupName >>= flip (processInterviewsToFiles groupName) interviews
+      where
+        groupName = group $ head interviews
+
+    processInterviewsToFiles groupName fileName = sequence_ . writeToFile groupName fileName . runLDAOnInterviews groupName
+
+    writeToFile groupName fileName models =
+      setLocaleEncoding utf8 :
+      writeFile fileName groupName :
+      fmap (appendFile fileName . showResult) models
+
+    runLDAOnInterviews groupName = runLDA . singletonList . combineDocuments groupName . createDocuments 
+        
+    singletonList x = [x]
 
     runLDA documents = run $ Input documents saveIterations saveInterval numberOfTopics seed
 
@@ -47,6 +88,11 @@ runLDAPerSubfolder = readInterviewsPerSubfolder interviewDirectory >>= runParall
     runLDAOnInterviews = runLDA . createDocuments
 
     runLDA documents = run $ Input documents saveIterations saveInterval numberOfTopics seed
+
+combineDocuments :: String -> [Document] -> Document
+combineDocuments name = foldl1 combine
+  where
+    combine d1 d2 = Document name (Document.words d1 ++ Document.words d2) (Map.unionWith (+) (Document.wordCounts d1) (Document.wordCounts d2))
 
 fullDataSet :: String
 fullDataSet = "Full-Data-Set"
